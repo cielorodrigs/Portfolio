fb18928bd4f0f8c57019a611c682c708b8d5de57
cielorodrigs <cielorodrigs@gmail.com>
2023-01-17 17:39:42 -0300

    proyecto

---
+++ portafolio-main/src/components/portafolio/components/dataProyect.js
@@ -0,0 +1,57 @@
+
+import datalovers from '../../../assets/portafolio/datalovers.PN"C:\Users\MSI GF65\Downloads\portafolio-main\portafolio-main\src\components\portafolio\components\POKEDEX.jpeg"';
+import mdLinks from '"C:\Users\MSI GF65\Downloads\portafolio-main\portafolio-main\src\components\portafolio\components\compulsiva.jpg"';
+import portafolio from '"C:\Users\MSI GF65\Downloads\portafolio-main\portafolio-main\src\components\portafolio\components\WheatherApp.jpeg"';
+import burger from '"C:\Users\MSI GF65\Downloads\portafolio-main\portafolio-main\src\components\portafolio\components\RICK AND MORTY.jpeg"';
+import ecommerce from '"C:\Users\MSI GF65\Downloads\portafolio-main\portafolio-main\src\components\portafolio\components\ecommerce.jpeg"'
+
+export const proyect = [
+  {
+    id: 1,
+    name: "E-COMMERCE",
+    github: "https://github.com/cielorodrigs/Entregable-6",
+    demo: "https://merry-lily-52aa09.netlify.app/#/",
+    filter: "JAVASCRIPT",
+    lenguage:
+      " Javascript , CSS , Html, GitHub proyect, Deployed in Netlify ",
+    image:
+      ecommerce,
+  },
+  {
+    id: 2,
+    name: "WEATHER APP",
+    github: "https://aquamarine-axolotl-e7f545.netlify.app/",
+    demo: "https://aquamarine-axolotl-e7f545.netlify.app/",
+    filter: "REACT",
+    lenguage: "Javascript, HTML, React, CSS, GitHub proyect, Deployed in Netlify ",
+    image: portafolio,
+  },
+  {
+    id: 3,
+    name: "POKEDEX",
+    github: "https://github.com/cielorodrigs/Entregable-5 ",
+    demo: "https://precious-clafoutis-93a08f.netlify.app/#/pokedex",
+    filter: "JAVASCRIPT",
+    lenguage: "Javascript, HTML, React, CSS, GitHub proyect, Deployed in Netlify",
+    image:datalovers,
+  },
+  {
+    id: 4,
+    name: "3D PROYECTS",
+    
+    demo: "https://drive.google.com/drive/folders/16eSn6etZazvcGPqUfDTKcNnnqReZ79qc?usp=share_link",
+    filter: "Architectural",
+    lenguage: "AutoCad, SketchUp Pro, Lumion",
+    image:mdLinks ,
+  },
+
+  {
+    id: 5,
+    name: "RICK AND MORTY",
+    github: "https://github.com/cielorodrigs/Entregable-3",
+    demo: "https://jovial-marshmallow-8e7701.netlify.app/",
+    filter: "REACT",
+    lenguage: "Javascript, HTML, React, CSS, GitHub proyect, Deployed in Netlify",
+    image:burger,
+  },
+];
---
+++ portafolio-main/src/components/skills/components/listIcon.js
@@ -0,0 +1,81 @@
+export const iconFront = [
+  {
+    id: 1,
+    icon: "html5",
+    prefix: "fab",
+    name: "html",
+  },
+  {
+    id: 2,
+    icon: "css3-alt",
+    prefix: "fab",
+    name: "css",
+  },
+  {
+    id: 3,
+    icon: "js",
+    prefix: "fab",
+    name: "js",
+  },
+  
+];
+
+export const iconFrame = [
+  {
+    id: 1,
+    icon: "react",
+    prefix: "fab",
+    name: "react",
+  },
+    
+  {
+    id: 2,
+    icon: "Compass-Drafting",
+    prefix: "fab",
+    name: "AutoCad",
+  },
+  {
+    id: 3,
+    icon: "Cube",
+    prefix: "fab",
+    name: "SketchUp Pro",
+  },
+  {
+    id: 4,
+    icon: "Head-Side-Goggles",
+    prefix: "fab",
+    name: "Lumion",
+  }
+
+];
+
+export const listBD = [
+  
+  {
+    id: 1,
+    icon: "node",
+    prefix: "fab",
+    name: "node",
+  },
+    {
+    id:2 ,
+    icon: "git",
+    prefix: "fab",
+    name: "git",
+  },
+  {
+    id: 3,
+    icon: "github",
+    prefix: "fab",
+    name: "gitHub",
+  },
+  {
+    id: 5,
+    icon: "npm",
+    prefix: "fab",
+    name: "npm",
+  },
+  
+
+
+];
